be91a12b586d658be32985585962a34e6046d90d
Jyothi <jyothiakkina345@gmai.com>
2025-08-14 23:49:03 +0530

    in userController.js updated deleteUser part

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,12 +76,11 @@ const updateUser = async (req, res) => {
 // @desc Delete user by ID
 const deleteUser = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findByIdAndDelete(req.params.id);
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        await user.remove();
         res.status(200).json({ message: "User deleted successfully" });
     } catch (error) {
         res.status(500).json({ message: error.message });
